c55bba92d1da9cdbda64d774c06367222df66540
Valen Rionald <vrionald@gmail.com>
2024-04-16 21:06:05 +0700

    Feat(Auth): Add Route Register, Login, GetMe dan Logout

--- routes/router.go
+++ routes/router.go
@@ -6,9 +6,16 @@ import (
 )
 
 func SetupTaskRoutes(app *fiber.App) {
+	app.Post("/register", controller.RegisterUser)
+	app.Post("/login", controller.LoginUser)
+
+	app.Use(controller.Authenticate)
+	app.Get("/getme", controller.GetMe)
 	app.Get("/tasks", controller.GetAllTask)
 	app.Get("/task/get/:id", controller.GetTaskById)
 	app.Post("/task/insert", controller.InsertTask)
 	app.Put("/task/update/:id", controller.UpdateTask)
 	app.Delete("/task/delete/:id", controller.DeleteTask)
+
+	app.Post("/logout", controller.LogoutUser)
 }
