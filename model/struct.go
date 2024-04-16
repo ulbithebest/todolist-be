50d8e7211cff01a5234267f42c0b89078000caa2
Valen Rionald <vrionald@gmail.com>
2024-04-16 15:00:30 +0700

    Feat(Struct): Tambah field user di task

--- model/struct.go
+++ model/struct.go
@@ -2,6 +2,7 @@ package model
 
 type Task struct {
 	IdTask    int    `gorm:"primaryKey;column:id_task" json:"id_task"`
+	IdUser    int    `gorm:"column:id_user" json:"is_user"`
 	Judul     string `gorm:"column:judul" json:"judul"`
 	Deskripsi string `gorm:"column:deskripsi" json:"deskripsi"`
 	DueDate   string `gorm:"column:due_date" json:"due_date"`
